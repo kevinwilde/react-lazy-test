abb800b84d375244d22cc47a437e5488bc339fa9
kevinwilde <kjwilde7@gmail.com>
2018-11-12 20:38:39 -0800

    using react.lazy with mini css extract plugin

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const LazyComponent = React.lazy(() => import('./LazyComponent'))
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <React.Suspense fallback={<div>loading...</div>}>
+          <LazyComponent />
+        </React.Suspense>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
---
+++ src/LazyComponent.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+import './lazy.css'
+
+export default function(props) {
+    return <div class="red">This is lazily loaded!</div>
+}
